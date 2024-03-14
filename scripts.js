ebe00a27098804730fc77618ea92b1900c6f1002
AndresRwboop123 <rojasvivancoandres@gmail.com>
2024-03-13 21:34:17 -0500

    Cambios de actualizacion

--- scripts.js
+++ scripts.js
@@ -1,29 +1,35 @@
 /*alert("Hola este es mi Javascript");*/
 
 document.addEventListener("DOMContentLoaded", function(event) {
-let contenidoTitulo="Nombre";
+/*let contenidoTitulo="Nombre";
 
-let titulo=document.querySelector(".jota");
+
+
+
+let titulo=document.querySelector(".jota");  */
 
 /*let titulo=document.getElementsByClassName(".fa-solid fa-truck-monster");*/
 
-titulo.innerHTML=contenidoTitulo;
+/*titulo.innerHTML=contenidoTitulo;*/
 
 /* me ayudo esto https://es.quora.com/Por-qu%C3%A9-la-sentencia-document-querySelector-h1-me-devuelve-null-En-mi-documento-HTML-hay-elementos-h1-y-lo-mismo-me-pasa-con-cualquier-otro-elemento-clase-o-id-Qu%C3%A9-estoy-haciendo-mal-o-me-falta-alg%C3%BAn*/
 
 
 
  /*console.log(titulo);*/
+
+ /*
 let textoTitulo= titulo.innerText;
 console.log(textoTitulo);
+*/
 
-
+/*
 if(textoTitulo=="Nombre"){
     titulo.innerHTML="  Mi Web"}
     else{
         console.log("no se cumple")
     } 
-    
+ */   
 /////* FUNCIONES *////
 
 
@@ -48,6 +54,19 @@ parrafo.innerText=cambiarTexto(nombress,ciudad,gusto);
 
 
 
+
+
+
+
+
+
+
+
+const nav =document.querySelector(".nav");
+
+window.addEventListener("scroll",function(){
+    nav.classList.toggle("active",window.scrollY>0)
+})
 
 
 
